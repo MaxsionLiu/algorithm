764064f0d0834c841888c1f62fdd1cc21330a19d
Liu Shuiyuan <1210257083@qq.com>
2020-08-27 15:32:51 +0800

    144. 二叉树的前序遍历

--- leetcode.js
+++ leetcode.js
@@ -63,3 +63,34 @@ var maxArea = function(height) {
       }
       return max
   };
+
+
+// 144. 二叉树的前序遍历
+
+ // Definition for a binary tree node.
+  function TreeNode(val) {
+      this.val = val;
+      this.left = this.right = null;
+  }
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var preorderTraversal = function(root) {
+      let result  = []
+ 
+ 
+      function helper (node) {
+         if(!node){
+             return
+         }
+         result.push(node.val)
+         helper(node.left)
+         helper(node.right)
+      }
+ 
+      helper(root)
+   
+      return result
+ };
