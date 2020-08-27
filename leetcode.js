eec22c689ce4a86816a1241e7c1257378ba19225
Liu Shuiyuan <1210257083@qq.com>
2020-08-27 15:28:03 +0800

    盛最多水的容器

--- leetcode.js
+++ leetcode.js
@@ -41,3 +41,25 @@ var removeDuplicates = function(nums) {
    }
    return pre + 1
 }
+
+// 11. 盛最多水的容器
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+      let i = 0
+      let j = height.length - 1
+      let max = 0
+      while(i < j) {
+  
+          const currTotal = (j - i ) * Math.min(height[i],height[j])
+          max = currTotal > max ?  currTotal : max
+          if(height[i] > height[j]) {
+              j--
+          } else {
+              i++
+          } 
+      }
+      return max
+  };
