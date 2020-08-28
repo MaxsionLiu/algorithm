8c7aef989e434933dc4c11244899c78eafdd02e1
Liu Shuiyuan <1210257083@qq.com>
2020-08-28 09:16:39 +0800

    104. 二叉树的最大深度

--- leetcode.js
+++ leetcode.js
@@ -123,3 +123,26 @@ var strStr = function(haystack, needle) {
       }
       return -1
   };
+
+//   104. 二叉树的最大深度
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+      if (!root || typeof root.val === 'undefined') {
+          return 0
+      }
+      if (root.left && root.root) {
+          return 1
+      }
+  
+      return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
