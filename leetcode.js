f05ebc7135cf878febb3543e25f2218d8257ea1e
Liu Shuiyuan <1210257083@qq.com>
2020-08-28 09:14:30 +0800

    8. 实现 strStr()

--- leetcode.js
+++ leetcode.js
@@ -94,3 +94,32 @@ var preorderTraversal = function(root) {
    
       return result
  };
+
+//  28. 实现 strStr()
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+      if(!needle) return 0
+      const needleLen = needle.length
+  
+      for(let i = 0; i < haystack.length;i++) {
+  
+          if(haystack[i] === needle[0] &&  (i + needleLen - 1) < haystack.length  ) {
+  
+              let same = true
+              for(let j = 0; j < needle.length; j++){
+                  if(needle[j] != haystack[i+j]){
+                      same = false
+                      break
+                  }
+              }
+              if(same) {
+               return i
+              }
+          }
+      }
+      return -1
+  };
