f63fc9ecfdebfa617649bcaa3f614a6069ebf6f0
Liu Shuiyuan <1210257083@qq.com>
2020-08-31 22:17:32 +0800

    101. 对称二叉树

--- leetcode.js
+++ leetcode.js
@@ -195,3 +195,31 @@ var maxSubArray = function(nums) {
       }
       return max
 };
+
+// 101. 对称二叉树
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+      if (!root) {
+          return true
+      }
+      function helper(leftNode,rightNode) {
+          if(!leftNode && !rightNode) {
+              return true
+          }
+          if(!leftNode || !rightNode) {
+              return false
+          }
+          return leftNode.val === rightNode.val && helper(leftNode.left,rightNode.right) && helper(leftNode.right,rightNode.left)
+      }
+      return helper(root.left,root.right)
+  };
