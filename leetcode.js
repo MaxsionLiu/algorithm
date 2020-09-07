ffdbb3872f897fce5a8e0055c7ff07d3ca405ae2
Liu Shuiyuan <1210257083@qq.com>
2020-09-07 22:23:49 +0800

    111. 二叉树的最小深度

--- leetcode.js
+++ leetcode.js
@@ -611,3 +611,35 @@ var isBalanced = function(root) {
   
       return Math.abs(helper(root.left) - helper(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right)
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 111. 二叉树的最小深度
+var minDepth = function(root) {
+    
+      if(!root) {
+          return 0
+      }
+   
+      if(!root.left && !root.right) {
+          return 1
+      }
+   
+      let min = Infinity
+      if(root.left) {
+          min = Math.min(minDepth(root.left),min)
+      }
+       if(root.right) {
+          min = Math.min(minDepth(root.right),min)
+      }
+      return min + 1
+};
