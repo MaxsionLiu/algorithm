966dc49edfab67a8015c22f6056cf9a8c906eb96
Liu Shuiyuan <1210257083@qq.com>
2020-09-08 21:10:18 +0800

    112. 路径总和

--- leetcode.js
+++ leetcode.js
@@ -673,3 +673,30 @@ var generate = function(numRows) {
       nums.push(currNew)
       return nums
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+// 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，
+// 这条路径上所有节点值相加等于目标和。
+// 112. 路径总和
+var hasPathSum = function(root, sum) {
+      
+      if(!root) {
+          return false
+      }
+  
+      if (!root.left && !root.right) {
+          return sum === root.val
+      }
+      return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+};
