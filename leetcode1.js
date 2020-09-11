1ec9efed7f2316f9e0f06367a7ae7fcfc774c9a9
Liu Shuiyuan <1210257083@qq.com>
2020-09-11 22:16:55 +0800

    155. 最小栈

--- leetcode1.js
+++ leetcode1.js
@@ -115,3 +115,49 @@ var hasCycle = function(head) {
   
       return true
 };
+
+/**
+ * initialize your data structure here.
+ */
+// 155. 最小栈
+// 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+// push(x) —— 将元素 x 推入栈中。
+// pop() —— 删除栈顶的元素。
+// top() —— 获取栈顶元素。
+// getMin() —— 检索栈中的最小元素。
+
+   var MinStack = function() {
+      this.stack = []
+      this.minStack = [Infinity]
+   };
+   
+   /** 
+    * @param {number} x
+    * @return {void}
+    */
+   MinStack.prototype.push = function(x) {
+       this.stack.push(x)
+       this.minStack.push(Math.min(x,this.minStack[this.minStack.length - 1]))
+   };
+   
+   /**
+    * @return {void}
+    */
+   MinStack.prototype.pop = function() {
+     this.stack.pop()
+     this.minStack.pop()
+   };
+   
+   /**
+    * @return {number}
+    */
+   MinStack.prototype.top = function() {
+       return this.stack[this.stack.length - 1]
+   };
+   
+   /**
+    * @return {number}
+    */
+   MinStack.prototype.getMin = function() {
+       return this.minStack[this.minStack.length - 1]
+   };
