1161411ce5e7d11f55817f25d98dcd984c6cb457
Liu Shuiyuan <1210257083@qq.com>
2020-09-12 21:54:24 +0800

    160. 相交链表

--- leetcode1.js
+++ leetcode1.js
@@ -161,3 +161,28 @@ var hasCycle = function(head) {
    MinStack.prototype.getMin = function() {
        return this.minStack[this.minStack.length - 1]
    };
+
+   /**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+// 160. 相交链表
+var getIntersectionNode = function(headA, headB) {
+      // 清除高度差
+      let pA = headA, pB = headB
+      while(pA || pB) {
+          if(pA === pB) return pA
+          pA = pA === null ? headB : pA.next
+          pB = pB === null ? headA : pB.next
+      }
+      return null
+  };
