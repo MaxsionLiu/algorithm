e28ad6935381a5a258fe08f47bbca17ce6655704
Liu Shuiyuan <1210257083@qq.com>
2020-09-17 21:37:14 +0800

    196. 删除重复的电子邮箱

--- leetcode1.js
+++ leetcode1.js
@@ -348,3 +348,9 @@ var hammingWeight = function(n) {
         }
       return sum
 };
+
+// 196. 删除重复的电子邮箱
+// delete p1 FROM Person p1,
+//     Person p2
+// WHERE
+//     p1.Email = p2.Email AND p1.Id > p2.Id
