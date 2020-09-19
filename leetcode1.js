4e7da951afc3095bfac0c89b4af5339834abe1d4
Liu Shuiyuan <1210257083@qq.com>
2020-09-19 10:57:08 +0800

    202. 快乐数

--- leetcode1.js
+++ leetcode1.js
@@ -422,3 +422,37 @@ var removeElements = function(head, val) {
       }
       return flagNode.next
   };
+
+  /**
+ * @param {number} n
+ * @return {boolean}
+ */
+// 202. 快乐数
+// 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数
+// 输入：19
+// 输出：true
+// 解释：
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+var isHappy = function(n) {
+      let slow = n
+      let fast = step(n)
+      while(fast !== 1 && slow !== fast) {
+          slow = step(slow)
+          fast = step(step(fast))
+      }
+  
+  
+      return fast == 1
+  };
+  
+var step = function(n) {
+      let result = 0
+      while ( n > 0) {
+            result += (n%10) * (n%10)
+            n = parseInt(n / 10)
+      }
+      return result
+}  
