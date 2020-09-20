22daef862e529f2bbcddcf475e77e4ea1bf2a7b6
Liu Shuiyuan <1210257083@qq.com>
2020-09-20 14:51:30 +0800

    217. 存在重复元素

--- leetcode1.js
+++ leetcode1.js
@@ -485,3 +485,23 @@ var reverseList = function(head) {
    
       return pre
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+// 给定一个整数数组，判断是否存在重复元素。
+// 如果任意一值在数组中出现至少两次，函数返回 true 。
+// 如果数组中每个元素都不相同，则返回 false
+// 217. 存在重复元素
+var containsDuplicate = function(nums) {
+      let hash = {}
+      for(let i = 0; i < nums.length; i++){
+          if(hash[nums[i]]) {
+              return true
+          }else {
+              hash[nums[i]] = 1
+          }
+      }
+      return false
+ };
