f737760f33451aef70c3c5781a17d1144c8f37d1
Liu Shuiyuan <1210257083@qq.com>
2020-09-22 21:24:11 +0800

    235. 二叉搜索树的最近公共祖先

--- leetcode1.js
+++ leetcode1.js
@@ -598,4 +598,37 @@ var isPalindrome = function(head) {
       }
       return true
    };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+// 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+// 235. 二叉搜索树的最近公共祖先
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+// 输出: 6 
+// 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+// 输出: 2
+// 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+var lowestCommonAncestor = function(root, p, q) {
+      if (root.val < p.val && root.val < q.val) {
+          return lowestCommonAncestor(root.right,p,q)
+      } else if(root.val > p.val && root.val > q.val) {
+          return lowestCommonAncestor(root.left,p,q)
+      } else {
+          return root
+      }
+  };
    
