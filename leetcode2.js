6b39b63535c6a635ab7ca0fa19790159a683ba0c
Liu Shuiyuan <1210257083@qq.com>
2020-09-23 17:26:28 +0800

    263. 丑数

--- leetcode2.js
+++ leetcode2.js
@@ -51,3 +51,29 @@ var binaryTreePaths = function(root) {
 var addDigits = function(num) {
       return (num - 1) % 9 + 1;
   };
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+// 263. 丑数
+// 编写一个程序判断给定的数是否为丑数。
+// 丑数就是只包含质因数 2, 3, 5 的正整数。(1也是)
+var isUgly = function(num) {
+      if (num <= 0) {
+          return false
+      }
+      if (num === 1) {
+          return true
+      }
+      if (num % 2 === 0) {
+          return isUgly(num / 2)
+      }
+      if (num % 3 === 0) {
+          return isUgly(num / 3)
+      }
+      if (num % 5 === 0) {
+          return isUgly(num / 5)
+      }
+      return false
+  };
