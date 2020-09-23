33765f4a7982a97c1a9ade889c8f10787e554095
Liu Shuiyuan <1210257083@qq.com>
2020-09-23 16:23:12 +0800

    257. 二叉树的所有路径

--- leetcode2.js
+++ leetcode2.js
@@ -10,3 +10,34 @@ var isAnagram = function(s, t) {
       }
       return s.split('').sort().join('') === t.split('').sort().join('')
   };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+// 257. 二叉树的所有路径
+var binaryTreePaths = function(root) {
+      let result = [] 
+      function helper(root, s) {
+           if(root) {
+               s += root.val
+               if (!root.left && !root.right) {
+                   result.push(s)
+               } else {
+                   s += '->'
+                   helper(root.left, s)
+                   helper(root.right, s)
+               }
+           }
+       } 
+   
+       helper(root, '')
+       return result
+   };
