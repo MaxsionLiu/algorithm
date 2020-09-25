084e45f1742abe99c7b5585ec2b40ddce48507ed
Liu Shuiyuan <1210257083@qq.com>
2020-09-25 17:34:27 +0800

    283. 移动零

--- leetcode2.js
+++ leetcode2.js
@@ -134,3 +134,27 @@ var solution = function(isBadVersion) {
          return helper(0,n)
       };
   };
+
+  /**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// 283. 移动零
+// 输入: [0,1,0,3,12]
+// 输出: [1,3,12,0,0]
+var moveZeroes = function(nums) {
+      for(let j = 0;j < nums.length; j++ )  {
+           if (nums[j] !== 0) {
+               continue
+           }
+           for(let i = nums.length - 1; i > j; i--) {
+               if (nums[i] === 0) {
+                   continue
+               }
+   
+               let temp = nums[j]
+               nums[j] = nums[i]
+               nums[i] = temp 
+           }
+       }
+   };
