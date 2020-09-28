256a7c40f212442d71f288ba77d0ef4ca3b20031
Liu Shuiyuan <1210257083@qq.com>
2020-09-28 21:33:54 +0800

    367. 有效的完全平方数

---
+++ leetcode3.js
@@ -0,0 +1,25 @@
+// 367. 有效的完全平方数
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+      if (num <= 1) {
+          return true
+      }
+      function helper(num, l,r) {
+             let mid = parseInt((l + r)/2)
+             if (mid * mid === num) {
+                 return  true
+             } else if(r - l == 1) {
+                 return  false
+             } else if(mid * mid < num) {
+                 return helper(num,mid,r)
+             } else {
+                 return helper(num,l,mid)
+             } 
+      }
+ 
+      return helper(num, 0, num)
+   
+ };
