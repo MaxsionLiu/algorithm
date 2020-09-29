78710d7e06d573624ded60163bce971af9731c40
Liu Shuiyuan <1210257083@qq.com>
2020-09-29 21:20:47 +0800

    374. 猜数字大小

--- leetcode3.js
+++ leetcode3.js
@@ -38,3 +38,37 @@ var getSum = function(a, b) {
       }
       return a
   };
+
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// 374. 猜数字大小
+var guessNumber = function(n) {
+      function helper(l,r) {
+          if (guess(l) === 0) {
+              return l
+          }
+          if (guess(r) === 0) {
+              return r
+          }
+          let mid = parseInt((l + r)/2)
+          if(guess(mid) === 0) {
+              return mid
+          } else if(guess(mid) === -1) {
+              return helper(l,mid)
+          } else {
+              return helper(mid,r)
+          }
+      }
+      return helper(0,n)
+  };
