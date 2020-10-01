ffcadc66f728d576d5c7dd06c2ff8ee4259e7610
Liu Shuiyuan <1210257083@qq.com>
2020-10-01 23:54:07 +0800

    412. Fizz Buzz

--- leetcode3.js
+++ leetcode3.js
@@ -175,4 +175,31 @@ var sumOfLeftLeaves = function(root) {
          return 0
       }
       return helper(root)
- }; 
+ };
+ 
+ /**
+ * @param {number} n
+ * @return {string[]}
+ */
+// 412. Fizz Buzz
+var fizzBuzz = function(n) {
+      let result = []
+      for(let i = 1; i <= n; i++) {
+  
+          if(i % 3 === 0 && i % 5 === 0) {
+             result.push('FizzBuzz')
+             continue
+          }
+          if( i % 3 === 0 ) {
+             result.push('Fizz')
+             continue
+          }
+          if( i % 5 === 0) {
+             result.push('Buzz')
+             continue
+          }
+          result.push(i + '')
+  
+      }
+      return result
+  };
