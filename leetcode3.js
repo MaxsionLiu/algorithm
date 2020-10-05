96e7499e6449ae8e234a6a48e3c2dbcdbdf0148b
Liu Shuiyuan <1210257083@qq.com>
2020-10-05 22:10:37 +0800

    455. 分发饼干

--- leetcode3.js
+++ leetcode3.js
@@ -324,3 +324,28 @@ var minMoves = function(nums) {
       })
       return count
   };
+
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+// 455. 分发饼干
+var findContentChildren = function(g, s) {
+      g.sort((a,b) => a - b)
+      s.sort((a,b) => a - b)
+   
+      let count = 0
+      let j = 0
+      for(let i = 0; i < g.length; i++) {
+         while(j < s.length) {
+             if(s[j] - g[i] >= 0) {
+               count++
+               j++
+               break
+             }
+           j++
+         }
+      }
+      return count
+   };
