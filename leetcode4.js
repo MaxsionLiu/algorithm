a15f82796493eeae01242d743ce100b8abfc75da
Liu Shuiyuan <1210257083@qq.com>
2020-10-08 22:39:36 +0800

    501. 二叉搜索树中的众数

--- leetcode4.js
+++ leetcode4.js
@@ -1,4 +1,53 @@
   /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 501. 二叉搜索树中的众数
+var findMode = function(root) {
+      if(!root) {
+          return []
+      }
+  
+      let answer = []
+      let maxCount = 0
+      let count = 0
+      let base
+      const update = (x) => {
+          if (x === base) {
+              ++count;
+          } else {
+              count = 1;
+              base = x;
+          }
+          if (count === maxCount) {
+              answer.push(base);
+          }
+          if (count > maxCount) {
+              maxCount = count;
+              answer = [base];
+          }
+      }
+  
+      function midOrderTree(node) {
+         if(!node) {
+            return
+         }
+         midOrderTree(node.left)
+         update(node.val)
+         midOrderTree(node.right)
+      }
+      midOrderTree(root)
+      return answer
+  };
+  
+  /**
  * // Definition for a Node.
  * function Node(val,children) {
  *    this.val = val;
