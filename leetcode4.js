78192a0d322d2cd8567656f0426c4add9afae7b4
Liu Shuiyuan <1210257083@qq.com>
2020-10-10 20:40:20 +0800

    530. 二叉搜索树的最小绝对差

--- leetcode4.js
+++ leetcode4.js
@@ -47,6 +47,40 @@ var findMode = function(root) {
       return answer
   };
   
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+// 530. 二叉搜索树的最小绝对差
+var getMinimumDifference = function(root) {
+      let arr = []
+      let min = Infinity
+      function helper(node) {
+          if(!node) {
+              return
+          }
+          helper(node.left)
+          arr.push(node.val)
+          helper(node.right)
+      }
+      helper(root)
+      for (let i = 0; i < arr.length - 1; i++) {
+          const temp = Math.abs(arr[i] - arr[i+1])
+          if(temp < min) {
+              min = temp
+          }
+      }
+      return min
+  };
+
   /**
  * // Definition for a Node.
  * function Node(val,children) {
