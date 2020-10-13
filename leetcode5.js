7bbf6c4137c683cffaec8733cfbd626fc612ece1
Liu Shuiyuan <1210257083@qq.com>
2020-10-13 22:38:59 +0800

    617. 合并二叉树

--- leetcode5.js
+++ leetcode5.js
@@ -32,3 +32,22 @@ var tree2str = function(t) {
       const str = helper(t)
       return str.substring(1,str.length-1)
   };
+
+  /**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+// 617. 合并二叉树
+var mergeTrees = function(t1, t2) {
+      if(!t1){
+          return t2
+      }
+      if(!t2){
+          return t1
+      }
+      let node = new TreeNode(t1.val + t2.val)
+      node.left = mergeTrees(t1.left, t2.left)
+      node.right = mergeTrees(t1.right, t2.right)    
+      return node
+  };
