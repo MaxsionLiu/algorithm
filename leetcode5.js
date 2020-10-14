28b3537667eb99324b8bc519fb31e4c09b8c4952
Liu Shuiyuan <1210257083@qq.com>
2020-10-14 20:26:55 +0800

    637. 二叉树的层平均值

--- leetcode5.js
+++ leetcode5.js
@@ -51,3 +51,37 @@ var mergeTrees = function(t1, t2) {
       node.right = mergeTrees(t1.right, t2.right)    
       return node
   };
+
+  /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 637. 二叉树的层平均值
+// 给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。
+var averageOfLevels = function(root) {
+      let nodeList = [root]
+      let result = []
+      while(nodeList.length > 0) {
+         let sum = 0
+         const len = nodeList.length
+         for(let i = 0; i < len; i++) {
+             const node = nodeList.shift()
+             sum += node.val
+             if(node.left) {
+                 nodeList.push(node.left)
+             }
+             if(node.right) {
+                 nodeList.push(node.right)
+             }
+         }
+         result.push(sum / len)
+      }
+      return result
+   };
