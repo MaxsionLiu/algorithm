2194b8094d1e019c4b73717c1e0eaa0c19c798a4
Liu Shuiyuan <1210257083@qq.com>
2020-10-21 22:55:09 +0800

    剑指 Offer 28. 对称的二叉树

--- leetcode8.js
+++ leetcode8.js
@@ -13,3 +13,24 @@ var mirrorTree = function(root) {
    
       return root
    };
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+// 剑指 Offer 28. 对称的二叉树
+var isSymmetric = function(root) {
+      if(!root) {
+          return true
+      }
+      function helper(L,R) {
+          if(!L && !R) {
+              return true
+          }
+          if(!L || !R || L.val !== R.val) {
+              return false
+          }
+          return helper(L.left,R.right) && helper(L.right,R.left)
+      }
+      return helper(root.left,root.right)
+  };
