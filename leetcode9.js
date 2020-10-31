d244c3e8b357d9708d7f7be744dea870595ca715
Liu Shuiyuan <1210257083@qq.com>
2020-10-31 22:31:55 +0800

    面试题 04.12. 求和路径

--- leetcode9.js
+++ leetcode9.js
@@ -311,3 +311,21 @@ var checkSubTree = function(t1, t2) {
             return left||right;            
         }
 };
+
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+// 面试题 04.12. 求和路径
+var pathSum = function(root, sum) {
+      if (!root) return 0;
+        return helper(root, sum) + pathSum(root.right, sum) + pathSum(root.left, sum);
+  
+        //以root 为根节点的时候，和为sum 的路径条数
+        function helper(root, sum) {
+              if (!root) return 0;
+              if (root.val === sum) return 1 + helper(root.left, sum-root.val) + helper(root.right, sum-root.val);
+              return helper(root.left, sum-root.val) + helper(root.right, sum-root.val);
+        }
+  };
