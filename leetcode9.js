c7e779696df2d9e233b625d87747d3880faa2b19
Liu Shuiyuan <1210257083@qq.com>
2020-11-01 18:50:59 +0800

    687. 最长同值路径

--- leetcode9.js
+++ leetcode9.js
@@ -329,3 +329,24 @@ var pathSum = function(root, sum) {
               return helper(root.left, sum-root.val) + helper(root.right, sum-root.val);
         }
   };
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 687. 最长同值路径
+var longestUnivaluePath = function(root) {
+      let res = 0
+      const univaluePath = (root) => {
+        if (!root) return 0
+        const left = univaluePath(root.left)
+        const right = univaluePath(root.right)
+        let leftPath = 0, rightPath = 0
+        if (root.left && root.left.val == root.val) leftPath = left + 1
+        if (root.right && root.right.val == root.val) rightPath = right + 1
+        res = Math.max(res, leftPath + rightPath)
+        return Math.max(rightPath, leftPath)
+      }
+      univaluePath(root)
+      return res
+    };
