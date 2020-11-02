0aa04f5636744cd6970b94126d4301ccfac7822a
Liu Shuiyuan <1210257083@qq.com>
2020-11-02 22:08:31 +0800

    889. 根据前序和后序遍历构造二叉树

--- leetcode9.js
+++ leetcode9.js
@@ -435,3 +435,22 @@ var pruneTree = function(root) {
       }
       return helper(root)
   };
+
+  /**
+ * @param {number[]} pre
+ * @param {number[]} post
+ * @return {TreeNode}
+ */
+// 889. 根据前序和后序遍历构造二叉树
+var constructFromPrePost = function(pre, post) {
+      if(pre.length == 0){
+          return null;
+      };
+  
+      let tmp = pre[0];
+      let index = post.indexOf(pre[1]);
+      let root = new TreeNode(tmp);
+      root.left = constructFromPrePost(pre.slice(1,index+2),post.slice(0,index+1));
+      root.right = constructFromPrePost(pre.slice(index+2),post.slice(index+1,post.length-1));
+      return root;
+  };
