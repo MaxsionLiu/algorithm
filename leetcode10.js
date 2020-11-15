8094fc58a9e0007d4b9f61561b3f979f8c13be58
Liu Shuiyuan <1210257083@qq.com>
2020-11-15 20:01:09 +0800

    236. 二叉树的最近公共祖先

--- leetcode10.js
+++ leetcode10.js
@@ -48,3 +48,24 @@ var kthSmallest = function(root, k) {
       helper(root)
       return result[k-1]
   };
+
+  /**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+// 236. 二叉树的最近公共祖先
+var lowestCommonAncestor = function(root, p, q) {
+      if (root == p || root == q) return root;
+      let left = null
+      let right = null
+      if(root.left) {
+          left = lowestCommonAncestor(root.left,p,q)
+      }
+      if(root.right) {
+          right = lowestCommonAncestor(root.right,p,q)
+      }
+      if (left && right) return root;
+      return left ? left : right;
+  };
