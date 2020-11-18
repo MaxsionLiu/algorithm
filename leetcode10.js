64418cd331e5f2c1174ac9efa407f70d7a1ac728
Liu Shuiyuan <1210257083@qq.com>
2020-11-18 22:04:05 +0800

    863. 二叉树中所有距离为 K 的结点

--- leetcode10.js
+++ leetcode10.js
@@ -182,3 +182,58 @@ var convertBST = function(root) {
   
       return helper(root)
   };
+
+  /**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} K
+ * @return {number[]}
+ */
+// 863. 二叉树中所有距离为 K 的结点
+var distanceK = function(root, target, k) {
+      if(!root) return [];
+      let targetNode = null;
+      let res = [];
+      let paths = [];
+  
+      // 找到target节点，存储到targetNode中
+      dfs(root, target);
+      // 从当前节点向下寻找
+      getdownDis(targetNode, k);
+      console.log('111111111111111111111111111111111111111111')
+      // 从当前节点向上寻找
+      while(targetNode.parent && k>0){
+          targetNode = targetNode.parent;
+          getdownDis(targetNode, --k);
+      }
+  
+      // 辅助函数
+      function dfs(_root, target){
+          if(!_root || targetNode) return;
+          if(_root.val === target.val){
+              targetNode = _root;
+          }
+          if(_root.left){
+              _root.left.parent = _root;
+              dfs(_root.left, target);
+          }
+          if(_root.right){
+              _root.right.parent = _root;
+              dfs(_root.right, target);
+          }
+      }
+  
+      // 辅助函数
+      function getdownDis(node, k){
+          if(node === null || paths.indexOf(node) !== -1) return;
+          paths.push(node);
+          if(k>0){
+              getdownDis(node.left, k-1);
+              getdownDis(node.right, k-1);  
+          }else if(k === 0){
+              res.push(node.val);
+          }
+      }
+  
+      return res;
+  };
