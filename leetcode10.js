11103adcf87c06dd726caade8f9473e96e2774c2
Liu Shuiyuan <1210257083@qq.com>
2020-11-26 22:12:03 +0800

    951. 翻转等价二叉树

--- leetcode10.js
+++ leetcode10.js
@@ -531,3 +531,20 @@ var maxLevelSum = function(root) {
    
       return idx + 1
   };
+
+  /**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+// 951. 翻转等价二叉树
+var flipEquiv = function(root1, root2) {
+      if(!root1 && !root2) {
+          return true
+      }
+      if(!(root1 && root2)) {
+          return false
+      }
+      return root1.val == root2.val && ((flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right)) ||
+      (flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left)) )
+  };
