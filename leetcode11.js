2d92fb78ce658f7ec26cf9bbb3f8d1b7f279aa11
Liu Shuiyuan <1210257083@qq.com>
2020-11-30 21:14:12 +0800

    1047. 删除字符串中的所有相邻重复项

--- leetcode11.js
+++ leetcode11.js
@@ -17,3 +17,27 @@ var pathSum = function(root, sum) {
       let rootNum = helper(root,sum)
       return rootNum + pathSum(root.left,sum) +  pathSum(root.right,sum)
   };
+
+  /**
+ * @param {string} S
+ * @return {string}
+ */
+// 1047. 删除字符串中的所有相邻重复项
+var removeDuplicates = function(S) {
+      let arr = S.split('')
+      let result = []
+      let len = arr.length
+      for(let i = 0; i < len; i++) {
+          const c = arr.shift()
+          if(result.length > 0) {
+             if(result[result.length - 1] === c) {
+                 result.pop()
+             } else {
+                 result.push(c)
+             }
+          }else{
+              result.push(c)
+          }
+      }
+      return result.join('')
+  };
