7fa90a94b38fa6fb3436b1980f7642e5fcb27c00
Liu Shuiyuan <1210257083@qq.com>
2020-12-02 21:52:49 +0800

    682. 棒球比赛

--- leetcode12.js
+++ leetcode12.js
@@ -18,3 +18,28 @@ var makeGood = function(s) {
       }
       return stack.join('')
     };
+
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+// 682. 棒球比赛
+var calPoints = function(ops) {
+      let stack = []
+      for(let i = 0; i < ops.length; i++) {
+          const ch = ops[i]
+          if(ch === 'C') {
+              stack.pop()
+          }else if(ch === 'D') {
+              const pre = stack[stack.length - 1]
+              stack.push(pre * 2)
+          }else if(ch === '+') {
+              const  pre = stack[stack.length - 1]
+              const prePre = stack[stack.length - 2]
+              stack.push(pre + prePre)
+          }else {
+              stack.push(Number(ch))
+          }
+      }
+      return stack.reduce((pre,curr) => pre + curr)
+  };
