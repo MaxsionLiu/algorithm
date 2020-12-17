489451fbf170430ff6cf423c6611d1465c1d09ad
Liu Shuiyuan <1210257083@qq.com>
2020-12-17 22:54:40 +0800

    剑指 Offer 35. 复杂链表的复制

--- sort.js
+++ sort.js
@@ -141,3 +141,25 @@ var reverseList = function(head) {
       }
       return reverseNode
   };
+
+  /**
+ * @param {Node} head
+ * @return {Node}
+ */
+// 剑指 Offer 35. 复杂链表的复制
+var copyRandomList = function(head) {
+      if(!head) return null;
+      let m = new Map();
+      let node = head;
+      while(node) {
+          m.set(node,new Node(node.val));
+          node = node.next;
+      }
+      node = head;
+      while(node) {
+          m.get(node).next = node.next ? m.get(node.next) : null;
+          m.get(node).random = node.random ? m.get(node.random) : null;
+          node = node.next;
+      }
+      return m.get(head)
+  };
