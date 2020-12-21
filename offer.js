afafcc9fc4285182c69b6af3ae3d75f4671f738e
Liu Shuiyuan <1210257083@qq.com>
2020-12-21 19:36:23 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

--- offer.js
+++ offer.js
@@ -223,3 +223,17 @@ var fib = function(n) {
       }
       return arr[n+1]
   };
+
+  /**
+ * @param {number} n
+ * @return {number}
+ */
+// 剑指 Offer 10- II. 青蛙跳台阶问题
+var numWays = function(n) {
+      let arr = [1,1,2]
+      for(let i = 3; i <= n;i++) {
+          arr[i] = arr[i-1] + arr[i-2]
+          arr[i] %= 1000000007;
+      }
+      return arr[n]
+   };
