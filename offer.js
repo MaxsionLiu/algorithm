0c22f907dfd681f1ebe50b5a681d19d104b2074a
Liu Shuiyuan <1210257083@qq.com>
2020-12-22 22:59:15 +0800

    1137. 第 N 个泰波那契数

--- offer.js
+++ offer.js
@@ -246,3 +246,28 @@ var numWays = function(n) {
       }
       return curr
    };
+
+   /**
+ * @param {number} n
+ * @return {number}
+ */
+// 1137. 第 N 个泰波那契数
+var tribonacci = function(n) {
+      if(n === 0) {
+          return 0
+      }
+      if(n <= 2) {
+          return 1
+      }
+      let t0 = 0
+      let t1 = 1
+      let t2 = 1
+   
+      for(let i = 3; i <= n;i++) {
+          const curr = t0 + t1 + t2
+          t0 = t1
+          t1 = t2
+          t2 = curr
+      }
+      return t2
+   };
